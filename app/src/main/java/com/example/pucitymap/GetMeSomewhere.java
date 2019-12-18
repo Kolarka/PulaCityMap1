952995367eb8525e0dde08faa21963fc4459ebc3
Kolarka <tkolar@unipu.hr>
2019-12-19 00:40:02 +0100

    Commit 2

--- app/src/main/java/com/example/pucitymap/GetMeSomewhere.java
+++ app/src/main/java/com/example/pucitymap/GetMeSomewhere.java
@@ -1,14 +1,19 @@
 package com.example.pucitymap;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class GetMeSomewhere extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_me_somewhere);
-    }
+    public class GetMeSomewhere extends AppCompatActivity {
+
+        private static final String TAG1 = "GetMeSomewhere";
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_get_me_somewhere);
+
+
+        }
+
+
 }
+
