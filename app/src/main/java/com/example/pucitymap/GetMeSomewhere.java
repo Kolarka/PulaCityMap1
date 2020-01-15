8fef7cf6d5fe29bb52b66518487856796301ee21
Kolarka <tkolar@unipu.hr>
2020-01-15 11:55:18 +0100

    This is commit 11

--- app/src/main/java/com/example/pucitymap/GetMeSomewhere.java
+++ app/src/main/java/com/example/pucitymap/GetMeSomewhere.java
@@ -1,5 +1,8 @@
 package com.example.pucitymap;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +15,19 @@ public class GetMeSomewhere extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_me_somewhere);
 
+        ImageButton getmesomewhere = (ImageButton) findViewById(R.id.Im33);
+        getmesomewhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGMS2();
+            }
+        });
 
+    }
+
+    private void openGMS2() {
+        Intent intentGMS = new Intent(this, GetMeSomewhere2.class);
+        startActivity(intentGMS);
     }
 
 
--- app/src/main/java/com/example/pucitymap/GetMeSomewhere2.java
+++ app/src/main/java/com/example/pucitymap/GetMeSomewhere2.java
@@ -1,8 +1,11 @@
 package com.example.pucitymap;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GetMeSomewhere2 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class GetMeSomewhere2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_me_somewhere2);
+        ImageButton get_me_somewhere = (ImageButton) findViewById(R.id.Im303);
+        get_me_somewhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMAP_direction();
+            }
+        });
+    }
+
+    private void openMAP_direction() {
+        Intent intentGMS2 = new Intent(this, Map_Direction.class);
+        startActivity(intentGMS2);
     }
 }
