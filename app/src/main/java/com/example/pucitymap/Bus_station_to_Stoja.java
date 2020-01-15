33592c52c705ed1c450dacaaddbfaada369c6962
Kolarka <tkolar@unipu.hr>
2020-01-15 10:25:28 +0100

    This is commit 9

---
+++ app/src/main/java/com/example/pucitymap/Bus_station_to_Stoja.java
@@ -0,0 +1,14 @@
+package com.example.pucitymap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Bus_station_to_Stoja extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_station_to__stoja);
+    }
+}
---
+++ app/src/main/java/com/example/pucitymap/Stoja_to_Bus_station.java
@@ -0,0 +1,14 @@
+package com.example.pucitymap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Stoja_to_Bus_station extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stoja_to__bus_station);
+    }
+}
